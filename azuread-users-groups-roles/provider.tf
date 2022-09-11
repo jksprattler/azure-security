f5ab9fd6cbf95c929e306430bebabeceacfda3ac
jksprattler <jenna.sprattler@gmail.com>
2022-09-11 12:08:04 -0500

    azuread updates

--- azuread-users-groups-roles/provider.tf
+++ azuread-users-groups-roles/provider.tf
@@ -1,10 +1,12 @@
 terraform {
   required_providers {
     azuread = {
-      source = "hashicorp/azuread"
+      source  = "hashicorp/azuread"
+      version = "2.28.1"
     }
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
+      version = "3.22.0"
     }
   }
 }
