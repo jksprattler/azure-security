da48f3c77fc9de361b6b924e64ae112d55c7a198
jksprattler <jenna.sprattler@gmail.com>
2023-01-29 13:00:48 -0600

    update access.tf

--- azuread-users-groups-roles/access.tf
+++ azuread-users-groups-roles/access.tf
@@ -7,33 +7,6 @@ data "azurerm_resource_group" "artgroup" {
   name = azurerm_resource_group.artgroup.name
 }
 
-# Azure custom role definitions
-resource "azurerm_role_definition" "Custom_storage_tfstate_role_def" {
-  name        = "CustomStoragetfStateRole"
-  scope       = "${data.azurerm_subscription.current.id}/resourceGroups/centralus-jennasrunbookstf/providers/Microsoft.Storage/storageAccounts/jennasrunbookstfstate"
-  description = "Custom storage role definition"
-
-  permissions {
-    actions = [
-      "Microsoft.Storage/storageAccounts/listKeys/action",
-      "Microsoft.Storage/storageAccounts/regeneratekey/action",
-      "Microsoft.Storage/storageAccounts/blobServices/generateUserDelegationKey/action"
-    ]
-    not_actions = []
-  }
-}
-
-# Provision Azure custom role assignments
-resource "azurerm_role_assignment" "Custom_storage_tfstate_role" {
-  scope              = "${data.azurerm_subscription.current.id}/resourceGroups/centralus-jennasrunbookstf/providers/Microsoft.Storage/storageAccounts/jennasrunbookstfstate"
-  role_definition_id = azurerm_role_definition.Custom_storage_tfstate_role_def.role_definition_resource_id
-  principal_id       = azuread_group.Engineering.object_id
-  description        = "Custom storage role for managing tf state blob"
-
-  depends_on = [azurerm_role_definition.Custom_storage_tfstate_role_def]
-
-}
-
 # Provision Azure built-in role assignments
 resource "azurerm_role_assignment" "Reader_role_all" {
   scope                = data.azurerm_subscription.current.id
