dd3a9402f4c1338f972fda3ee03a13e3532d8b98
jksprattler <jenna.sprattler@gmail.com>
2023-01-29 11:31:43 -0600

    comments and edits

--- azuread-users-groups-roles-pt2/groups.tf
+++ azuread-users-groups-roles-pt2/groups.tf
@@ -10,18 +10,6 @@ resource "azuread_group" "Readers" {
   }
 }
 
-resource "azuread_group" "Engineering" {
-  display_name = "Engineering"
-  #owners           = [data.azuread_client_config.current.object_id]
-  security_enabled = true
-  types            = ["DynamicMembership"]
-
-  dynamic_membership {
-    enabled = true
-    rule    = "user.department -eq \"Engineering\""
-  }
-}
-
 resource "azuread_group" "Art" {
   display_name = "Art"
   #owners           = [azuread_user.current.object_id]
@@ -32,4 +20,17 @@ resource "azuread_group" "Art" {
     enabled = true
     rule    = "user.department -eq \"Art\""
   }
+}
+
+resource "azuread_group" "Engineering" {
+  display_name     = "Engineering"
+  security_enabled = true
+}
+
+# Loop through all users and for each user assigned to the Engineering department assign their membership to the Engineering group:
+resource "azuread_group_member" "Engineering" {
+  for_each = { for u in local.users : u.mail_nickname => u if u.department == "Engineering" }
+
+  group_object_id  = azuread_group.Engineering.id
+  member_object_id = azuread_user.users[each.key].id
 }
--- azuread-users-groups-roles-pt2/users.tf
+++ azuread-users-groups-roles-pt2/users.tf
@@ -1,16 +1,23 @@
 locals {
+  # Capture the custom domain you want assigned to your User Principals
   domain_name = data.azuread_domains.aad_domains.domains.1.domain_name
   users       = csvdecode(file("${path.module}/users.csv"))
 }
 
+# Loop through the list of users in the block mapped to each line of the users.csv file
+# The users.csv file requires: first_name,last_name,mail_nickname,preferred_language
+# usage_location is required with Microsoft licenses assigned to a user
 resource "azuread_user" "users" {
+  # Define each user key by their unique mail nickname since potential first name duplicates are high
   for_each = { for user in local.users : user.mail_nickname => user }
   user_principal_name = format(
     "%s@%s",
     lower(each.value.mail_nickname),
     local.domain_name
   )
-
+  
+  # The new user will be created with an auto-generated password using the following pattern in all lowercase letters in a single string: 
+  # lastname + first letter of first name + numerical value for length of first name + !123
   password = format(
     "%s%s%s!123",
     lower(each.value.last_name),
